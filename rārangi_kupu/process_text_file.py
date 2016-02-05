477dc853420818718c920c07cc96835b00ea6297
orotau <greenbay.graham@gmail.com>
2016-02-06 12:09:34 +1300

    progress with regex

--- rārangi_kupu/process_text_file.py
+++ rārangi_kupu/process_text_file.py
@@ -19,24 +19,114 @@ def process_text_file(file_id):
     text_file_path = text_files_path + file_id + os.extsep + TEXT_EXTENSION
 
     # Get the non-compound maori words using regex
+    regex_maori_word = r"""
+    \b                      # capture whole words only
 
-    regex_maori_word = """
-    (                       # grouped so we can use the +
+    (?:
+
+    (?:                     # group so we can use the +
+                            # non-capturing so the whole word is returned
     ng|                     # needs to be before the n
     wh|                     # needs to be before the w and h
-    [aeiouāēīōūhkmnprtw]
-    )                       
+    [hkmnprtw]
+    )
+    ?                       # 0 or 1 times
+    
+    (?:
+    [aeiouāēīōū]
+    )
+    
+    )                      
     +                       # one or more of the above
+    \b
     """
+
+    regex_open_compound_candidate = r"""
+
+    (?:
+    [A-ZĀĒĪŌŪ]              # capital letter to start
+    [a-zāēīōū]+             # one or more alphanumeric characters
+    \s+                     # one or more spaces
+    )
+    +                       # one or more
+
+    (?:
+    \b
+    [A-ZĀĒĪŌŪ]              # capital letter to start
+    [a-zāēīōū]+             # one or more alphanumeric characters
+    \b
+    )
+
+    (?!
+    -                       # Don't bump into a closed compound
+    )
+
+    """
+
+    regex_closed_compound = r"""
+
+    (?:    
+
+    (?:
+    \w+                     # one word 
+    )
+
+    (?:  
+    -                       # dash
+    \w+                     # one word
+    )+                      # one or more times
+
+    )
+    """
+
+    regex_capitalised_word = r"""
+
+    (?:
+                     
+    (?:
+    \b
+    [AEIOUĀĒĪŌŪHKMNPRTW]    #capital letter to start
+    \w+                     #one or more alphanumeric characters
+    \b
+    )
+
+    )
+    """
+
+
+
+
+    test = "Waiho te mīti kia āta hū i roto i te hōpane mō tētahi 30 meneti atu anō."
 
     with open(text_file_path, 'r') as f:
         for line_number, line in enumerate(f):
-            print("Line " + str(line_number))
-            words = re.finditer(regex_maori_word, line, re.VERBOSE | re.IGNORECASE)
-            for w in words:
-                print(w)
-            print("==================")
+            maori_words = re.findall(regex_maori_word, line, re.VERBOSE | re.IGNORECASE)
+            all_words = re.findall(r"\w+", line)
+            open_compound_candidates = re.findall(regex_open_compound_candidate, \
+                                                  line, re.VERBOSE)
 
+            closed_compounds = re.findall(regex_closed_compound, \
+                                                  line, re.VERBOSE)
+
+            capitalised_words = re.findall(regex_capitalised_word, \
+                                                  line, re.VERBOSE)
+                                                    
+            '''
+            if maori_words != all_words:
+                for word in all_words:
+                    if word not in maori_words:
+                        if word == "whakpaipai":
+                            print(all_words)
+            '''
+            for open_compound_candidate in open_compound_candidates:
+                print("start")
+                print(line_number, line)
+                print(open_compound_candidate)
+                print("end")
+                print("\n\n")
+
+            #for closed_compound in closed_compounds:
+            #    print(closed_compound)
 
 if __name__ == '__main__':
 
